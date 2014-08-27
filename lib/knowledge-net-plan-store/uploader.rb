c0a897426026e985e877b844c2c23bed264e8fd4
Kaid <kaid@kaid.me>
2014-08-27 16:44:44 +0800

    added removed store_dir method in Uploader

--- lib/knowledge-net-plan-store/uploader.rb
+++ lib/knowledge-net-plan-store/uploader.rb
@@ -2,10 +2,6 @@ module KnowledgeNetPlanStore
   class Uploader < CarrierWave::Uploader::Base
     storage :file
 
-    def store_dir
-      "public/images/#{base}-#{model.id.to_s}"
-    end
-
     def default_url
       "/assets/defaults/#{base}.png"
     end

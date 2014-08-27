cc81422c1e3c09b64cd05a517b9e6aba209c4f63
Kaid <kaid@kaid.me>
2014-08-27 11:58:45 +0800

    added default images for tutorials and topics

--- lib/knowledge-net-plan-store/uploader.rb
+++ lib/knowledge-net-plan-store/uploader.rb
@@ -1,5 +1,17 @@
 module KnowledgeNetPlanStore
   class Uploader < CarrierWave::Uploader::Base
     storage :file
+
+    def store_dir
+      "public/images/#{base}-#{model.id.to_s}"
+    end
+
+    def default_url
+      "/assets/defaults/#{base}.png"
+    end
+
+    def base
+      model.class.name.split("::").last.underscore
+    end
   end
 end
